ff0ff830a654b6d1d823219b05d78a79b6934856
wanhee27 <chowh727@gmail.com>
2024-03-21 17:44:05 +0900

    fix: list모듈에 누락된 모듈 추가

--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -6,9 +6,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Card } from 'src/card/entities/card.entity';
 import { Board } from 'src/board/entities/board.entity';
 import { BoardService } from 'src/board/board.service';
+import { User } from 'src/user/entities/user.entity';
+import { Shared } from 'src/user/entities/shared.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([List, Card, Board])],
+  imports: [TypeOrmModule.forFeature([List, Card, Board, User, Shared])],
   controllers: [ListController],
   providers: [ListService, BoardService],
 })
